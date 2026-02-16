bcbe6d52df5b93d17d7a9dfc7e28d187f566e9db
Anuksha Shirgave <anukshashirgave2003@gmail.com>
2026-02-16 19:32:00 +0530

    fixed main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://0.0.0.0:8080",
         os.getenv("FRONTEND_URL"),  # Railway env variable
     ],
     allow_credentials=True,
